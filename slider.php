ce1eaebb954094af68aa455f6e552943faad194a
Rahula Palu Caleffi <rahula_dd@hotmail.com>
2022-06-15 14:01:00 +0100

    First Commit
    Functional, using demo images

---
+++ slider.php
@@ -0,0 +1,84 @@
+
+<link rel="modulepreload" href="<?php echo get_template_directory_uri();?>/Slider-Tour-Swipper/assets/module-preload.js">
+<script type="module" crossorigin src="<?php echo get_template_directory_uri();?>/Slider-Tour-Swipper/assets/module-slider.js"></script>
+<style>
+    .swiper{
+        height:400px;
+    }
+</style>
+<!-- Slider main container -->
+<div class="travel-slider">
+  <div class="travel-slider-planet" style="transform: translate(-50%, -50%) rotate(0deg); transition-duration: 0ms;">
+        <img src="wp-content/themes/twentytwentytwo/Slider-Tour-Swipper/assets/images/3909361.png">
+        <!-- <div class="travel-slider-cities travel-slider-cities-8">
+          <img src="images/usa.svg">
+          <img src="images/england.svg">
+          <img src="images/france.svg">
+          <img src="images/italy.svg">
+          <img src="images/russia.svg">
+          <img src="images/egypt.svg">
+          <img src="images/india.svg">
+          <img src="images/japan.svg">
+        </div> -->
+  </div>
+  <div class="swiper">
+        <div class="swiper-wrapper" >
+          <div class="swiper-slide swiper-slide-visible swiper-slide-active" style="margin-right: 64px;">
+            <img src="wp-content/themes/twentytwentytwo/Slider-Tour-Swipper/assets/images/usa.jpg" class="travel-slider-bg-image">
+            <div class="travel-slider-content">
+              <div class="travel-slider-title">United States</div>
+              <div class="travel-slider-subtitle">8,295 properties</div>
+            </div>
+          </div>
+          <div class="swiper-slide swiper-slide-visible swiper-slide-next" style="margin-right: 64px;">
+            <img src="wp-content/themes/twentytwentytwo/Slider-Tour-Swipper/assets/images/england.jpg" class="travel-slider-bg-image">
+            <div class="travel-slider-content">
+              <div class="travel-slider-title">England</div>
+              <div class="travel-slider-subtitle">1,110 properties</div>
+            </div>
+          </div>
+          <div class="swiper-slide" style="margin-right: 64px;">
+            <img src="wp-content/themes/twentytwentytwo/Slider-Tour-Swipper/assets/images/france.jpg" class="travel-slider-bg-image">
+            <div class="travel-slider-content">
+              <div class="travel-slider-title">France</div>
+              <div class="travel-slider-subtitle">314 properties</div>
+            </div>
+          </div>
+          <div class="swiper-slide" style="margin-right: 64px;">
+            <img src="wp-content/themes/twentytwentytwo/Slider-Tour-Swipper/assets/images/italy.jpg" class="travel-slider-bg-image">
+            <div class="travel-slider-content">
+              <div class="travel-slider-title">Italy</div>
+              <div class="travel-slider-subtitle">1,200 properties</div>
+            </div>
+          </div>
+          <div class="swiper-slide" style="margin-right: 64px;">
+            <img src="wp-content/themes/twentytwentytwo/Slider-Tour-Swipper/assets/images/russia.jpg" class="travel-slider-bg-image">
+            <div class="travel-slider-content">
+              <div class="travel-slider-title">Russia</div>
+              <div class="travel-slider-subtitle">12,231 properties</div>
+            </div>
+          </div>
+          <div class="swiper-slide" style="margin-right: 64px;">
+            <img src="wp-content/themes/twentytwentytwo/Slider-Tour-Swipper/assets/images/egypt.jpg" class="travel-slider-bg-image">
+            <div class="travel-slider-content">
+              <div class="travel-slider-title">Egypt</div>
+              <div class="travel-slider-subtitle">505 properties</div>
+            </div>
+          </div>
+          <div class="swiper-slide" style="margin-right: 64px;">
+            <img src="wp-content/themes/twentytwentytwo/Slider-Tour-Swipper/assets/images/india.jpg" class="travel-slider-bg-image">
+            <div class="travel-slider-content">
+              <div class="travel-slider-title">India</div>
+              <div class="travel-slider-subtitle">2,300 properties</div>
+            </div>
+          </div>
+          <div class="swiper-slide" style="margin-right: 64px;">
+            <img src="wp-content/themes/twentytwentytwo/Slider-Tour-Swipper/assets/images/japan.jpg" class="travel-slider-bg-image">
+            <div class="travel-slider-content">
+              <div class="travel-slider-title">Japan</div>
+              <div class="travel-slider-subtitle">1,700 properties</div>
+            </div>
+          </div>
+        </div>
+  </div>
+</div>
